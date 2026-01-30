ebc6d6e2c9e0f01a07ed72dc3bd3585709830edd
yugal1107 <yugal1107@gmail.com>
2026-01-30 10:16:10 +0530

    ADDED some unoptimized code

--- CSES/DP/CoinCombinations2.cpp
+++ CSES/DP/CoinCombinations2.cpp
@@ -3,32 +3,54 @@
 using namespace std;
 typedef long long ll;
 
-vector<vector<ll>> dp;
+vector<vector<int>> dp;
 
 ll mod = 1e9+7;
 
-ll recur(int t, int idx, vector<int>& coins){
+// Showing TLE using recursion
+// ll recur(int t, int idx, vector<int>& coins){
+//     int n = coins.size();
+
+//     if (t<0 || idx == n) return 0;
+//     if (t==0) return 1;
+
+//     if (dp[t][idx]!=-1) return dp[t][idx]; 
+
+//     ll take = recur(t-coins[idx], idx, coins);
+
+//     ll nt = recur(t, idx+1, coins);
+
+//     return dp[t][idx] = (take + nt)%mod;
+// }
+
+// ll solve(vector<int>& coins, int t){
+//     dp.assign(t+1, vector<int>(coins.size(), -1));
+//     return recur(t, 0, coins);
+// }
+
+// Using tabulation (This is also giving TLE)
+ll solve(vector<int>& coins, int t){
     int n = coins.size();
 
-    if (t<0 || idx == n) return 0;
-    if (t==0) return 1;
+    vector<vector<int>> dp(t+1, vector<int>(n, 0));
 
-    if (dp[t][idx]!=-1) return dp[t][idx]; 
+    for (int i=0 ; i<n ; i++){
+        dp[0][i]=1;
+    }
 
-    ll take = recur(t-coins[idx], idx, coins);
+    for (int sum = 1 ; sum <= t ; sum++){
+        for (int i = 0 ; i<n ; i++){
+            dp[sum][i] = ((i-1>=0) ? dp[sum][i-1] : 0) + ((sum - coins[i]>=0) ? dp[sum - coins[i]][i] : 0);
+            dp[sum][i]%=mod;
+        }
+    }
 
-    ll nt = recur(t, idx+1, coins);
-
-    return dp[t][idx] = (take + nt)%mod;
-}
-
-//Using bottom-up DP approach
-ll solve(vector<int>& coins, int t){
-    dp.assign(t+1, vector<ll>(coins.size(), -1));
-    return recur(t, 0, coins);
+    return dp[t][n-1];
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     // your code goes here
     int n,x;
     cin >> n >> x;
