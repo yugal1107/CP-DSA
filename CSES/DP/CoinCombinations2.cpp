a7354a4ca9ce0f77f940c16beaa59a92c3e7d3bc
yugal1107 <yugal1107@gmail.com>
2026-02-02 05:15:37 +0000

    Some changes

--- CSES/DP/CoinCombinations2.cpp
+++ CSES/DP/CoinCombinations2.cpp
@@ -3,32 +3,51 @@
 using namespace std;
 typedef long long ll;
 
-vector<vector<ll>> dp;
+// vector<vector<ll>> dp;
 
 ll mod = 1e9+7;
 
-ll recur(int t, int idx, vector<int>& coins){
+// ll recur(int t, int idx, vector<int>& coins){
+//     int n = coins.size();
+
+//     if (t<0 || idx == n) return 0;
+//     if (t==0) return 1;
+
+//     if (dp[t][idx]!=-1) return dp[t][idx]; 
+
+//     ll take = recur(t-coins[idx], idx, coins);
+
+//     ll nt = recur(t, idx+1, coins);
+
+//     return dp[t][idx] = (take + nt)%mod;
+// }
+
+// //Using bottom-up DP approach
+// ll solve(vector<int>& coins, int t){
+//     dp.assign(t+1, vector<ll>(coins.size(), -1));
+//     return recur(t, 0, coins);
+// }
+
+ll solve(vector<int>& coins, int t){
     int n = coins.size();
 
-    if (t<0 || idx == n) return 0;
-    if (t==0) return 1;
+    vector<ll>dp(t+1, 0);
 
-    if (dp[t][idx]!=-1) return dp[t][idx]; 
+    dp[0]=1;
 
-    ll take = recur(t-coins[idx], idx, coins);
+    for (int i = 0; i<n ; i++){
+        for (int sum = 1 ; sum <= t ; sum++){
+            if (sum - coins[i]>=0) dp[sum]+=dp[sum - coins[i]];
+            if (dp[sum]>=mod) dp[sum]-=mod;
+        }
+    }
 
-    ll nt = recur(t, idx+1, coins);
-
-    return dp[t][idx] = (take + nt)%mod;
-}
-
-//Using bottom-up DP approach
-ll solve(vector<int>& coins, int t){
-    dp.assign(t+1, vector<ll>(coins.size(), -1));
-    return recur(t, 0, coins);
+    return dp[t];
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     // your code goes here
     int n,x;
     cin >> n >> x;
